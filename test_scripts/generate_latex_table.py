8339eb5f51fd2dda56a9581d2bddc14fb6960aa7
Ivan Mysin <imysin@mail.ru>
2020-12-05 03:24:02 +0300

    Work on generation parameters as latex table

--- test_scripts/generate_latex_table.py
+++ test_scripts/generate_latex_table.py
@@ -3,22 +3,34 @@ from neuron import h, load_mechanisms
 import matplotlib.pyplot as plt
 import os
 import pandas as pd
-
+from math import floor, log10
 h.load_file("stdgui.hoc")
 h.load_file("stdrun.hoc")
 
 load_mechanisms("../mods/")
+
+def f(number):
+    if number == 0:
+        return "0"
+    if number < 0:
+        return str(number)
+    sigfig = 2
+    return ("%.15f" % (round(number, int(-1 * floor(log10(number)) + (sigfig - 1))))).rstrip("0").rstrip(".")
 
 for cellfile in os.listdir("../cells/"):
     _, ext = os.path.splitext(cellfile)
     if ext != ".hoc": continue
     h.load_file("../cells/" + cellfile)
 
-rem_param = ["point_processes", "node_index", "na_ion", "sec", "v", "volume", "x", "ri", "k_ion", "ca_ion", "area"]
+rem_param = ["e_ch_Navaxonp", "point_processes", "node_index", "na_ion", "sec", "v", "volume", "x", "ri", "k_ion", "ca_ion", "area", "ena", "ek"]
 
 
 path2latextablefile = "/home/ivan/Документы/latex_supplement/pvbastable.tex"
 celltype = "pvbas"
+
+caption = "PV basket cells parameters"
+label = "ca1_" + celltype + "_cell_parameters"
+# ca1_pyramidal_cell_parameters
 
 cells_params = {
         "pyr" : {
@@ -95,7 +107,7 @@ for sec in cell.all:
     if not (name == "soma" or name =="axon"):
         number = int(number) + 1
         name = name + " " + str(number)
-    # print(name)
+
     names.append(name)
 
 
@@ -129,8 +141,10 @@ for sec_idx, sec in enumerate(cell.all):
 
             
 
-pameters = list(set(pameters))
-# print(pameters)
+pameters = sorted(list(set(pameters)))
+pameters.remove("e_ch_Navaxonp")
+
+
 table = pd.DataFrame(np.nan, index=pameters, columns=names) 
 table.index.name = "Parameters"
 
@@ -154,24 +168,45 @@ for sec_idx, sec in enumerate(cell.all):
 # table = table.T.drop_duplicates(inplace=False).T
 print(table) # = table
 
+funsc = [f for _ in range(len(table.index))]
+latex_lable = table.T.to_latex(na_rep=" --- ", longtable=True, caption=caption, label=label, formatters=funsc)
 
-latex_lable = table.to_latex(na_rep=" --- ", float_format="%.5f", longtable=True)
+
+latex_lable = latex_lable.replace("Navaxonp", "Na")
+# latex_lable = latex_lable.replace("e_", "E_")
 
 
 latex_lable = latex_lable.replace(r"\_ch\_", "")
-latex_lable = latex_lable.replace("cm", "C, $\\mu F / cm^2$")
-latex_lable = latex_lable.replace("eleak", "$E_L, mV$")
-latex_lable = latex_lable.replace("diam", "D, $\\mu m$")
-latex_lable = latex_lable.replace("\\\\\nL", "\\\\\nL, $\\mu m$")
-latex_lable = latex_lable.replace("Ra", "Ra, $ohm cm$")
+latex_lable = latex_lable.replace("cm", "$C, \linebreak \\mu F / cm^2$")
+latex_lable = latex_lable.replace("eleak", "$E_L, \linebreak mV$")
+latex_lable = latex_lable.replace("gmaxleak", "$g_L, \linebreak mS / cm^2$")
+latex_lable = latex_lable.replace("diam", "$D,\linebreak \\mu m$")
+latex_lable = latex_lable.replace("\\\\\nL", "\\\\\n$L,\linebreak \\mu m$")
+latex_lable = latex_lable.replace("Ra", "$Ra,\n ohm cm$")
 latex_lable = latex_lable.replace("CavL", "CaL")
 latex_lable = latex_lable.replace("CavN", "CaN")
 latex_lable = latex_lable.replace("Nav", "Na")
 latex_lable = latex_lable.replace("KvA", "KA")
 latex_lable = latex_lable.replace("KvC", "KC")
 
+
 latex_lable = latex_lable.replace("fast", "")
-latex_lable = latex_lable.replace("gmax", "$g_{max, }$")
+
+latex_lable_tmp = ""
+for tmp_idx, tmp in enumerate (latex_lable.split("&")):
+    if tmp.find("gmax") != -1:
+        tmp = tmp.replace("gmax", "$g_{max, ")
+        tmp += "}\linebreak \\  mS / cm^2$"
+    latex_lable_tmp += tmp + "&" 
+
+latex_lable = latex_lable_tmp[:-1]
+
+
+old_str = "l" + "r" * len(table.index)
+new_str = "|p{1.5cm}" * (len(table.index) + 1)
+# \begin{longtable}{lrrrrrrrrrrrrrr}
+
+latex_lable = latex_lable.replace(old_str, new_str)
 
 
 with open(path2latextablefile, "w") as texfile:
